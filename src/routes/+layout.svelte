c6ed7066c6ad1b8b3c6e011520b0c052fb08817d
TurgutKursun <turgut.reis.kursun@gmail.com>
2026-06-19 17:17:22 +0200

    remove ugly svg

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -60,14 +60,6 @@
 		</a>
 	</div>
 
-	<svg class="fixed bottom-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"
-		><path
-			fill="#cce0ff"
-			fill-opacity="0.8"
-			d="M0,128L48,154.7C96,181,192,235,288,261.3C384,288,480,288,576,250.7C672,213,768,139,864,106.7C960,75,1056,85,1152,106.7C1248,128,1344,160,1392,176L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-		></path></svg
-	>
-
 	<div class="fixed right-5 bottom-5 z-9999">
 		<Button.Root
 			class="h-12 items-center justify-center rounded-full bg-dark
